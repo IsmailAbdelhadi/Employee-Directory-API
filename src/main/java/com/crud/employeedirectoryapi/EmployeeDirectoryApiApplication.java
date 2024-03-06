feed86fadf0071c4e2c4d04ead0915c9ae84ff16
Ismail_Abdelhadi <ismaeela754@gmail.com>
2024-03-06 18:36:23 +0300

    first commit

---
+++ src/main/java/com/crud/employeedirectoryapi/EmployeeDirectoryApiApplication.java
@@ -0,0 +1,13 @@
+package com.crud.employeedirectoryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeDirectoryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeDirectoryApiApplication.class, args);
+	}
+
+}
