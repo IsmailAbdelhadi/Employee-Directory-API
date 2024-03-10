37ff4a96b0410d9915e51bfb48c88cf2a70de954
Ismail_Abdelhadi <ismaeela754@gmail.com>
2024-03-10 17:12:55 +0300

    Spring Data REST

--- src/main/java/com/crud/springboot/dao/EmployeeRepository.java
+++ src/main/java/com/crud/springboot/dao/EmployeeRepository.java
@@ -4,4 +4,5 @@ import com.crud.springboot.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+    // no need to write any code!
 }
